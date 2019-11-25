2e0b6319efee1e7ca5c7861ebd207577060924c2
Gragas Gorilla <ggorilla@ox-v3.21-school.ru>
2019-11-25 19:37:53 +0300

    что я вообще делаю

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,45 @@
 /*   By: ggorilla <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/16 18:40:55 by ggorilla          #+#    #+#             */
-/*   Updated: 2019/11/24 00:14:41 by ggorilla         ###   ########.fr       */
+/*   Updated: 2019/11/25 19:15:23 by ggorilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//написать инклуд и что-то про бафсайз
+#include "get_next_line.h"
 
 char		buf[BUFF_SIZE];
 static char	*new[12000];
 int			i;
 
-//read(...) < 0
-
-int		ft_symb_no(char *a, char b)
+int	ft_symb_no(char *a, char b)
 {
 	int i;
 
 	i = 0;
 	while (a[i] != b)
 		i++;
-	return i;
+	return (i);
 }
 
-int     get_next_line(const int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
+	if (fd < 0 || fd > 10240 || line == NULL || read(fd, NULL, 0) < 0)
+		return (-1); //случаи ошибок
 	while (read(fd, &buf, BUFF_SIZE) >= 0)
 	{
 		new[fd] = ft_strjoin(new[fd], buf);
-		if (ft_strchr(new[fd], "\n"))
+		if (ft_strchr(new[fd], '\n'))
 		{
 			i = ft_symb_no(new[fd], '\n');
-			line = ft_strncpy(line, new[fd], i);
+			*line = ft_strncpy(*line, new[fd], i);
 			new[fd] = ft_strsub(new[fd], i + 1, ft_strlen(new[fd]) - i - 1);
 			return (1);
 		}
-		else if (read(fd, &buf, BUFF_SIZE) == 0) //случай, когда весь файл - одна строка
+		else if (read(fd, &buf, BUFF_SIZE) == 0) //дочитали
 		{
-			line = ft_strcpy(line, new);
-			ft_strdel(new);
-			return (1);
+			*line = ft_strcpy(*line, new[fd]);
+			ft_strdel(new[fd]);
+			return (0);
 		}
 	}
 }
